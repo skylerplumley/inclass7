75f90bd2ec21a7dc5b6659caa107ebe6392ba073
Skyler Plumley <skylerplumley@gmail.com>
2025-02-26 13:17:04 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FadingTextAnimation(),
+    );
+  }
+}
+
+class FadingTextAnimation extends StatefulWidget {
+  @override
+  _FadingTextAnimationState createState() => _FadingTextAnimationState();
+}
+
+class _FadingTextAnimationState extends State<FadingTextAnimation> {
+  bool _isVisible = true;
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fading Text Animation'),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
+          child: Text(
+            'Hello, Flutter!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
